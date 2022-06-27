aaaa47e177663ef35973f92674de59ad837c4a3f
Shourjjo_Majumder <shourjjomajum@outlook.com>
2022-06-27 18:25:19 +0530

    Fixed line errors added wping and removed the src of portable version the bin of normal version will be put in a zip for the portable

--- src/app/Program.cs
+++ src/app/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Net;
+using System.Net.NetworkInformation;
 
 
 
 namespace TermBolt{
 	class Program {
+	
 		static void Main(string[] args)
 		{
 				System.Net.WebClient wc = new System.Net.WebClient();
@@ -13,8 +15,8 @@ Console.Title = "TermBolt";
 while (true) 
 {
 string latver = wc.DownloadString("https://raw.githubusercontent.com/shourgamer2/TermBolt/main/version.txt");
-if (latver.Trim().Contains("1.1.0") == true) {
-	Console.Write("    ");
+if (latver.Trim().Contains("1.1.1") == true) {
+	Console.WriteLine("    ");
 
 }
 else 
@@ -38,19 +40,30 @@ Console.WriteLine("Termbolt is a fast and usefull terminal | GitHub: https://git
 
 }
 if (inputpr.Contains("wfd") == true) {
-Console.Write("Enter The Url:");
+Console.WriteLine("Enter The Url:");
 string durl = Console.ReadLine();
-Console.Write("Enter The Folder and the filename you want to download:");
+Console.WriteLine("Enter The Folder and the filename you want to download:");
 string dir = Console.ReadLine();
 DownClient.DownloadFile(durl,dir);
-Console.Write("File is downloaded" + dir);
+Console.WriteLine("File is downloaded" + dir);
+
 
 
   
 
 
 }
+if (inputpr.Contains("wping") == true) {
+Console.WriteLine("Enter ip or url you want to ping.");
+string ip = Console.ReadLine();
+Ping wpingob = new Ping();
+PingReply reply = wpingob.Send(ip);
+Console.WriteLine("Status :  " + reply.Status + " \n Time : " + reply.RoundtripTime.ToString() + " \n Address : " + reply.Address);
+
+
+
 }
 		}
+	}
 	}
 }
