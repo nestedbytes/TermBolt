2c4f7c3df38ba51c7ba69bb5147f2198f142e919
Shourjjo_Majumder <shourjjomajum@outlook.com>
2022-07-13 19:31:12 +0530

    3.0.0

--- src/app/Program.cs
+++ src/app/Program.cs
@@ -15,28 +15,25 @@ namespace TermBolt{
 				WebClient DownClient = new WebClient ();
 				// Set title
 Console.Title = "TermBolt";
+
 // Loop
 while (true) 
 {
 	// Check For updates
-	// If on correct version
+	// If on cold version 
 string latver = wc.DownloadString("https://raw.githubusercontent.com/shourgamer2/TermBolt/main/version.txt");
-if (latver.Trim().Contains("2.1.1") == true) {
-	Console.WriteLine("    ");
+if (!latver.Trim().Contains("3.0.0") == true) {
+	Console.WriteLine("Version outdated go to https://github.com/shourgamer2/TermBolt to download");
 
 }
-// If on old version
-else 
-{
-	Console.WriteLine("Version outdated go to https://github.com/shourgamer2/TermBolt to download");
-}
+
 // Make the console text
 string userName = Environment.UserName;
 Console.Write(userName + "@" + "TermBolt:");
 // Help
 string inputpr = Console.ReadLine();
 if (inputpr.Contains("help") == true) {
-   Console.WriteLine("Commands. exit = closes the window | help = Get help and list of commands | about = about this terminal ! | wfd = download file from web | wping = ping a url or a site | calc = Do maths inside termbolt ! ");
+   Console.WriteLine("Commands. exit = closes the window | help = Get help and list of commands | about = about this terminal ! | wfd = download file from web | wping = ping a url or a site | calc = Do maths inside termbolt ! | cclear = Clears the terminal ");
 }
 // Exit
 if (inputpr.Contains("exit") == true) {
@@ -76,6 +73,12 @@ Console.WriteLine("Status :  " + reply.Status + " \n Time : " + reply.RoundtripT
 }
 if (inputpr.Contains("calc") == true) {
 caclc();
+
+
+
+}
+if (inputpr.Contains("cclear") == true) {
+Console.Clear();
 
 
 
