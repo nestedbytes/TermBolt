8766d9cc0efc4a9247b8b87d6a31ccb0901f9195
Shourjjo Majumder <shourjjomajum@outlook.com>
2022-07-21 19:51:13 +0530

    Create linux-install-termbolt.sh

---
+++ linux-install-termbolt.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Downloading zip"
+wget https://github.com/shourgamer2/TermBolt/releases/download/3.0.0/TermBolt-Linux.zip
+echo "Unzipping"
+unzip TermBolt-Linux.zip
+echo "Everything is done now run ./termbolt to open termbolt!"
