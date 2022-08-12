e83a624c803fe2b4023744a352a8c21c685fe4b7
Shourjjo Majumder <shourjjomajum@outlook.com>
2022-08-12 15:43:11 +0530

    Update linux-install-termbolt.sh

--- linux-install-termbolt.sh
+++ linux-install-termbolt.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-echo "Installing wget"
-sudo apt-get install wget
 echo "Downloading zip"
-wget https://github.com/shourgamer2/TermBolt/releases/download/3.0.0/TermBolt-Linux.zip
+curl https://github.com/shourgamer2/TermBolt/releases/download/3.0.0/TermBolt-Linux.zip --output TermBolt-Linux.zip
 echo "Installing unzip"
 sudo apt-get install unzip
 echo "Unzipping"
