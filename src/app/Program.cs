9f4d21e10ce2ef531ecd77857eae7373a168a077
Shourjjo Majumder <shourjjomajum@outlook.com>
2022-08-30 18:08:04 +0530

    Added ctime in help

--- src/app/Program.cs
+++ src/app/Program.cs
@@ -33,7 +33,7 @@ Console.Write(userName + "@" + "TermBolt:");
 // Help
 string inputpr = Console.ReadLine();
 if (inputpr.Contains("help") == true) {
-   Console.WriteLine("Commands. exit = closes the window | help = Get help and list of commands | about = about this terminal ! | wfd = download file from web | wping = ping a url or a site | calc = Do maths inside termbolt ! | cclear = Clears the terminal | pcu = Print the current user account's name | strt = start a process");
+   Console.WriteLine("Commands. exit = closes the window | help = Get help and list of commands | about = about this terminal ! | wfd = download file from web | wping = ping a url or a site | calc = Do maths inside termbolt ! | cclear = Clears the terminal | pcu = Print the current user account's name | strt = start a process | ctime = Get current time of your computer");
 }
 // Exit
 if (inputpr.Contains("exit") == true) {
@@ -101,7 +101,7 @@ Process.Start(ex);
 
 
 }
-
+// ctime
 if (inputpr.Contains("ctime") == true) {
 DateTime time = DateTime.Now;
 Console.WriteLine(time);
